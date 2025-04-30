b9983c63b5c5bea3bc2060883d41c1dd013eea2d
Kermit <k84101311@gmail.com>
2025-04-30 09:02:36 +0800

    4/29迴圈複習

--- src/main/java/org/example/Iteration.java
+++ src/main/java/org/example/Iteration.java
@@ -4,11 +4,16 @@ public class Iteration {
     public static void main(String[] args) {
         // 基本for迴圈：從0印到9
         System.out.println("基本for迴圈：");
-        for (int i = 0; i < 10; i++) {
+
+        for (int i = 0; i < 4; i++) {
+            if (i == 2) {
+                continue;
+            }
             System.out.println("計數: " + i);
         }
 
-        // 計算1到5的總和
+
+//        // 計算1到5的總和
         System.out.println("\n計算總和：");
         int sum = 0;
         for (int i = 1; i <= 5; i++) {
@@ -20,17 +25,43 @@ public class Iteration {
 
 
         //array 陣列 []
-        String[] fruits = {"apple", "banana", "peach", "pineapple"};
-//        System.out.println(fruits[0]);
-//        System.out.println(fruits[1]);
-//        System.out.println(fruits[2]);
-
-        for (int i = 0; i < fruits.length; i++) {
-            System.out.println(fruits[i]);
+        String[] fruitBox = {"apple", "banana", "peach", "pineapple"};
+        //1 basic for
+        for (int i = 0; i < 5; i++) {
+            System.out.println(fruitBox[i]);
         }
-        for (String fruit : fruits) {
+
+        //2 enhanced for ：不管長度多少 走完就對了！
+        //遍例
+        for (String fruit : fruitBox) {
+            if (fruit == "apple") {
+                continue;
+            }
             System.out.println(fruit);
         }
+
+
+        String[] customers = {"kermit", "henry", "jack", "joanne"};
+
+        for (String customer : customers) {
+            System.out.println(customer);
+        }
+
+        //宣告 double [] 裡面有六個數字 1.0 ,2.0...  enhanced for
+        double[] numbers = {1.0, 2.0, 3.0, 4.0, 5.0};
+        for (double number : numbers) {
+            if (number % 2 != 0) {
+                System.out.println("奇數：" + number);
+                continue;
+            }
+            System.out.println("偶數：" + number);
+        }
+        // 奇數：1.0
+        // 偶數：2.0
+        // 奇數：3.0
+        // 偶數：4.0
+        // 奇數：5.0
+
 
         // 提示：
         // 1. 使用雙層for迴圈
