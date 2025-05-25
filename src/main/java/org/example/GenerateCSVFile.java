0c898b0c14628fd0d4ecf90623216beae07f680f
Kermit <k84101311@gmail.com>
2025-05-25 10:28:36 +0800

    5/23 GenerateCSVFile.java

--- src/main/java/org/example/GenerateCSVFile.java
+++ src/main/java/org/example/GenerateCSVFile.java
@@ -5,24 +5,23 @@ import net.datafaker.Faker;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class GenerateCSVFile {
     public static void main(String[] args) {
         Faker faker = new Faker();
         String fileName = "user.csv";
         int numberOfUser = 100;
-
+        //try with resource
         try (
                 //MAC User
 //                FileWriter fileWriter = new FileWriter(fileName);
                 // Windows User
                 FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, UTF_8);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
 
                 BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
         ) {
-            bufferedWriter.write("名字, Email");
+            bufferedWriter.write("名字, Email, 電話");
             bufferedWriter.newLine();
             for (int i = 0; i < numberOfUser; i++) {
                 String name = faker.name().fullName();
